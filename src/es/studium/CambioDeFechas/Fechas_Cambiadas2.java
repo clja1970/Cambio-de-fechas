885128f08fc5f0fd72c4e231241b6a75fe42ff1c
clja1970 <pepecl2020@gmail.com>
2021-03-16 22:28:37 +0100

    Completo Cambio de fechas sin errores

--- src/es/studium/CambioDeFechas/Fechas_Cambiadas2.java
+++ src/es/studium/CambioDeFechas/Fechas_Cambiadas2.java
@@ -1,6 +1,5 @@
 package es.studium.CambioDeFechas;
 
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Frame;
@@ -10,49 +9,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.util.Scanner;
+
+import javax.swing.JButton;
 
 public class Fechas_Cambiadas2  implements ActionListener, WindowListener
 {
 	Frame ventana = new Frame();
-	Label lblFecha = new Label("Introduce una fecha Europea dd/mm/yyyy:");
+	Label lblFecha = new Label(" Dame una fecha Europea");
+	Label lblEuropea = new Label("dd/mm/yyyy:");
 	TextField txtFecha = new TextField(8);
-	Button btnTransformacion = new Button("Transformación");
+	JButton btnTransformacion = new JButton("Transformación");
 	TextField txtFechaTransformada= new TextField(8);
 	Label lblAmericana =new Label ("Fecha Americana");
-	
+
 	public Fechas_Cambiadas2()
 	{
 		ventana.setLayout(new FlowLayout());
 		ventana.setBackground(Color.getHSBColor(73,210,77));
 		ventana.add(lblFecha);
+		ventana.add(lblEuropea);
 		ventana.add(txtFecha);
 		ventana.add(btnTransformacion);
 		ventana.add(txtFechaTransformada);
 		ventana.add(lblAmericana);
-		ventana.setSize(250,150);
+		btnTransformacion.addActionListener(this);
+		ventana.setSize(200,190);
 		ventana.setResizable(true);
 		ventana.setLocationRelativeTo(null);
 		ventana.addWindowListener(this);
 		ventana.requestFocus();
 		ventana.setVisible(true);
 	}
-	
+
 	public static void main(String[] args)
 	{
-		
-		//Frame ventana = new Frame();
-		//Label lblFecha = new Label("Introduce una fecha Europea dd/mm/yyyy:");
-		//TextField txtFecha = new TextField(8);
-		//Button btnTransformacion = new Button("Transformación");
-		
-		//String fecha ="";
-		//System.out.println("Introduce una fecha europea ");
-		//Scanner teclado = new Scanner(System.in);
-		//fecha=teclado.next();
-		//String[] arrayFecha = fecha.split("/");
-		//System.out.println("La fecha introducida en formato Americano es: "+arrayFecha[2]+"-"+arrayFecha[1]+"-"+arrayFecha[0]);
-		//teclado.close();
 		new Fechas_Cambiadas2();
 	}
 
@@ -60,55 +50,64 @@ public class Fechas_Cambiadas2  implements ActionListener, WindowListener
 	public void windowActivated(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowClosed(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0)
 	{
-		// TODO Auto-generated method stub
-		
+		System.exit(0);
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0)
 	{
-		// TODO Auto-generated method stub
-		
+
+
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0)
+	public void actionPerformed(ActionEvent event)
 	{
-		// TODO Auto-generated method stub
-		
+		try {
+
+			if (btnTransformacion.equals(event.getSource()))
+			{
+				String[] arrayFecha = txtFecha.getText().split("/");
+				txtFechaTransformada.setText(arrayFecha[2]+"/"+arrayFecha[1]+"/"+arrayFecha[0]);
+			}
+		}catch (ArrayIndexOutOfBoundsException e)
+		{
+			txtFecha.setText("");
+		}
+
 	}
 }
