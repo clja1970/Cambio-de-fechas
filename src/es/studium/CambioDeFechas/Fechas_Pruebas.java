fd2b7f875b32bc4125f44d2333f2524809386910
clja1970 <pepecl2020@gmail.com>
2021-03-16 20:06:18 +0100

    Primeras pruebas

---
+++ src/es/studium/CambioDeFechas/Fechas_Pruebas.java
@@ -0,0 +1,17 @@
+package es.studium.CambioDeFechas;
+
+import java.util.Scanner;
+
+public class Fechas_Pruebas
+{
+	public static void main(String[] args)
+	{
+		String fecha ="";
+		System.out.println("Introduce una fecha europea ");
+		Scanner teclado = new Scanner(System.in);
+		fecha=teclado.next();
+		String[] arrayFecha = fecha.split("/");
+		System.out.println("La fecha introducida en formato Americano es: "+arrayFecha[2]+"-"+arrayFecha[1]+"-"+arrayFecha[0]);
+		teclado.close();
+	}
+}
